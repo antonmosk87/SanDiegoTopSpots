b279f478974d535127137a5548bb9e52625bfbd0
Anton <antonmosk87@gmail.com>
2017-03-06 15:36:05 -0800

    TopSpotsSanDiego Complete

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $.getJSON("https://raw.githubusercontent.com/OriginCodeAcademy/Cohort10/master/Projects/04-SanDiegoTopSpots/topspots.json",
+    function (data) {
+        var tr;
+        for (var i = 0; i < data.length; i++) {
+            tr = $('<tr/>');
+            var links = data
+            tr.append("<td>" + data[i].name + "</td>");
+            tr.append("<td>" + data[i].description + "</td>");
+            tr.append("<td>" + '<a href="https://www.google.com/maps/place/' + data[i].location + '">' + data[i].location + '</a>'+ "</td>");
+            $('table').append(tr);
+        }
+    });
+});
